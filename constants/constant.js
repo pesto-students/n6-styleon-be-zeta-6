b018375814c399cc0583b35c57a63401823bcb1a
Varun Prabhakaran <varunprabhakaran2209@gmail.com>
2021-09-06 12:22:55 +0530

    Constant file added for static messages

---
+++ constants/constant.js
@@ -0,0 +1,31 @@
+const GET_SUCCESS = "Successfully Fetched Data";
+const GET_FAILED = "Failed to Fetch Data";
+const POST_SUCCESS = "Successfully Created Data";
+const POST_FAILED = "Failed to Create Data";
+const UPDATE_SUCCESS = "Updated Successfully";
+const UPDATE_FAILED = "Failed to Update the Data";
+const DELETE_SUCCESS = "Deleted Successfully";
+const DELETE_FAILED = "Failed to Delete the Data";
+const TOKEN_NOT_SUPPLIED = "Authentication error: Auth Token is not supplied";
+const TOKEN_NOT_VALID = "Token is not valid";
+const SCHEMA_PRODUCT = "Product";
+const SCHEMA_REVIEW = "Review";
+const SCHEMA_SERVICE = "Service";
+const SCHEMA_SALON = "salon";
+
+module.exports = {
+    GET_SUCCESS,
+    GET_FAILED,
+    POST_SUCCESS,
+    POST_FAILED,
+    UPDATE_SUCCESS,
+    UPDATE_FAILED,
+    DELETE_SUCCESS,
+    DELETE_FAILED,
+    TOKEN_NOT_SUPPLIED,
+    TOKEN_NOT_VALID,
+    SCHEMA_PRODUCT,
+    SCHEMA_REVIEW,
+    SCHEMA_SERVICE,
+    SCHEMA_SALON
+};

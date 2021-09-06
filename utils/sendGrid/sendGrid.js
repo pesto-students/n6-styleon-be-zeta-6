b9931f6bfe867bc8eec4aa3f1621705dcfd09886
Varun Prabhakaran <varunprabhakaran2209@gmail.com>
2021-09-06 12:24:35 +0530

    Send grid added for email services

---
+++ utils/sendGrid/sendGrid.js
@@ -0,0 +1,146 @@
+const sgMail = require("@sendgrid/mail");
+const sendGridKeys = require("../../keys/sendGrid");
+
+sgMail.setApiKey(sendGridKeys.SENDGRID_API_KEY);
+
+/*
+    Method to send email via SendGrid
+*/
+const sendPurchaseEmail = (recepientEmail, recepientName) => {
+    //Msg Template
+    const msg = {
+        to: recepientEmail,
+        from: sendGridKeys.senderEmail,
+        template_id: "d-a9625667d71e47e5a8965bc4273d16c5",
+        subject: "Sending with dynamic email SendGrid is Fun",
+        // text: "and easy to do anywhere, even with Node.js haha",
+        // html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+        personalizations: [
+            {
+                to: { email: recepientEmail },
+                dynamic_template_data: {
+                    first_name: recepientName,
+                },
+            },
+        ],
+    };
+
+    let response;
+    sgMail
+        .send(msg)
+        .then(res => {
+            console.log("Email sent check now", res);
+            response = res;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    return response;
+};
+
+/*
+    Method to send email via SendGrid
+*/
+const sendRescheduledEmail = (recepientEmail, recepientName, orderID) => {
+    //Msg Template
+    const msg = {
+        to: recepientEmail,
+        from: sendGridKeys.senderEmail,
+        template_id: "d-a9625667d71e47e5a8965bc4273d16c5",
+        subject: "Sending with dynamic email SendGrid is Fun",
+        // text: "and easy to do anywhere, even with Node.js haha",
+        // html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+        personalizations: [
+            {
+                to: { email: recepientEmail },
+                dynamic_template_data: {
+                    first_name: recepientName,
+                    order_id: orderID,
+                },
+            },
+        ],
+    };
+
+    let response;
+    sgMail
+        .send(msg)
+        .then(res => {
+            console.log("Email sent check now", res);
+            response = res;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    return response;
+};
+
+const sendOrderCancelledUpdates = (recepientEmail, recepientName, orderID) => {
+    //Msg Template
+    const msg = {
+        to: recepientEmail,
+        from: sendGridKeys.senderEmail,
+        template_id: "d-a9625667d71e47e5a8965bc4273d16c5",
+        subject: "Sending with dynamic email SendGrid is Fun",
+        // text: "and easy to do anywhere, even with Node.js haha",
+        // html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+        personalizations: [
+            {
+                to: { email: recepientEmail },
+                dynamic_template_data: {
+                    first_name: recepientName,
+                    order_id: orderID,
+                },
+            },
+        ],
+    };
+
+    let response;
+    sgMail
+        .send(msg)
+        .then(res => {
+            console.log("Email sent check now", res);
+            response = res;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    return response;
+};
+
+const sendEmail = (recepientEmail, recepientName) => {
+    //Msg Template
+    const msg = {
+        to: recepientEmail,
+        from: sendGridKeys.senderEmail,
+        template_id: "d-a9625667d71e47e5a8965bc4273d16c5",
+        subject: "Sending with dynamic email SendGrid is Fun",
+        // text: "and easy to do anywhere, even with Node.js haha",
+        // html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+        personalizations: [
+            {
+                to: { email: recepientEmail },
+                dynamic_template_data: {
+                    first_name: recepientName,
+                },
+            },
+        ],
+    };
+
+    let response;
+    sgMail
+        .send(msg)
+        .then(res => {
+            console.log("Email sent check now", res);
+            response = res;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    return response;
+};
+
+module.exports = {
+    sendPurchaseEmail,
+    sendRescheduledEmail,
+    sendOrderCancelledUpdates,
+};
